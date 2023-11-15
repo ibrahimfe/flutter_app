53fb0244139cb2f1b7363ee9401fb357ee1425ce
ibrahimfe <ibrahiimm.f@gmail.com>
2023-11-15 16:35:29 +0700

    refactor the structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import "package:app/welcome.dart";
+import 'package:app/data_produk.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,150 +11,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter App",
-      home: Store(),
-    );
-  }
-}
-
-class Store extends StatelessWidget {
-  const Store({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Data Produk"),
-        actions: const [Icon(Icons.replay_outlined)],
-      ),
-      body: ListView(
-        children: const [
-          Divider(
-            height: 0,
-          ),
-          Card(
-            elevation: 12,
-            child: ListTile(
-              title: Text("Filter Produk"),
-              trailing: Icon(Icons.cancel_outlined),
+      home: Login(),
+      routes: {
+        '/list': (context) => const Store(
+              email: '',
             ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("1"),
-              title: Text("Mangga"),
-              subtitle: Text("Harga: Rp. 5000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("2"),
-              title: Text("Jeruk"),
-              subtitle: Text("Harga: Rp. 6000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("3"),
-              title: Text("Apel"),
-              subtitle: Text("Harga: Rp. 5500"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("4"),
-              title: Text("Semangka"),
-              subtitle: Text("Harga: Rp. 9000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("5"),
-              title: Text("Durian"),
-              subtitle: Text("Harga: Rp. 15000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("6"),
-              title: Text("Tomat"),
-              subtitle: Text("Harga: Rp. 2000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("7"),
-              title: Text("Stroberi"),
-              subtitle: Text("Harga: Rp. 7500"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("8"),
-              title: Text("Pisang"),
-              subtitle: Text("Harga: Rp. 25000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text("9"),
-              title: Text("Buah Naga"),
-              subtitle: Text("Harga: Rp. 55000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Text(
-                "10",
-              ),
-              title: Text("Melon"),
-              subtitle: Text("Harga: Rp. 10000"),
-              trailing: Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-            ),
-          ),
-        ],
-      ),
+      },
     );
   }
 }
---
+++ lib/welcome.dart
@@ -0,0 +1,96 @@
+import "package:flutter/material.dart";
+import "package:app/data_produk.dart";
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController emailController = TextEditingController();
+
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Email"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Password"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        if (emailController.text == "untirta@untirta.ac.id" &&
+                            passwordController.text == "untirtajawara") {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Store(
+                                      email: emailController.text,
+                                    )),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Invalid Credentials')),
+                          );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
